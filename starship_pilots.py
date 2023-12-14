6e8103ae3c2e8d6aab19f8b7258bed41440bd77a
Aymen Shallal <aymenwork2020@gmail.com>
2023-12-14 16:46:31 +0000

    self. changes

--- starship_pilots.py
+++ starship_pilots.py
@@ -6,11 +6,8 @@ class Starship_Pilots(swapi_call.StarWarsAPI):
 
     def __init__(self):
         super().__init__()
-        starship_results = self.get_starships_data()['results']
+        self.starship_results = self.get_starships_data()['results']
         self.list = []
-        for i in starship_results:
+        for i in self.starship_results:
             self.list.append({i['name']: i['pilots']})
-
-##
-
 
