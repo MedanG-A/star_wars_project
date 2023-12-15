0f91097f4476b8b00757468382e913cfc70da86a
Jesse <jr2g17@soton.ac.uk>
2023-12-15 15:11:05 +0000

    adding comments to replace urls feature

--- replace_URLs_with_ObjectID.py
+++ replace_URLs_with_ObjectID.py
@@ -15,26 +15,29 @@ class ReplacePilotUrls(sp.Starship_Pilots, sw.StarWarsAPI):
         super().__init__()
 
     def pilot_ids(self):
+        # pilot_names will be populated with the names of the pilots from the starships database
         pilot_names = []
-        for starships in self.list:
-            pilots = list(starships.values())[0]
+        for starships in self.list:             # self.list is a list of dictionaries. Each dict is a starship.
+            pilots = list(starships.values())[0]        # Extracting the list of pilot names from the starship dict.
             if len(pilots) >= 1:
                 get_starship_name = list(starships.keys())
-                starship = get_starship_name[0]
+                starship = get_starship_name[0]                 # Extracting the name of the starship.
                 driver_dict = {starship: []}
                 for pilot in pilots:
-                    pilot_details = requests.get(pilot)
-                    json_pilot_details = pilot_details.json()
+                    pilot_details = requests.get(pilot)         # Requesting the pilot data from the pilot URL
+                    json_pilot_details = pilot_details.json()       # Converting the response to json
                     driver_dict[starship].append(json_pilot_details['name'])
                 pilot_names.append(driver_dict)
 
+        # A list of dictionaries with starship as the key and a list of pilot ids.
         pilot_objectids = []
         for starships in pilot_names:
             get_starship_name = list(starships.keys())
-            starship = get_starship_name[0]
+            starship = get_starship_name[0]             # Extracting the starship name
             pilot_ids_dict = {starship: []}
-            name_list = list(starships.values())[0]
+            name_list = list(starships.values())[0]     # Extracting the list of pilot names
             for name in name_list:
+                # Finding the pilot_id from the characters collection
                 character = db.characters.find_one({"name": name}, {"_id": 1})
                 if character:
                     pilot_ids_dict[starship].append(character["_id"])
